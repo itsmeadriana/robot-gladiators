a529f68e46549a4003332ab610341c7492d81ad8
Adriana <84536824+itsmeadriana@users.noreply.github.com>
2021-07-06 18:27:26 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,13 +3,25 @@ var playerHealth = 100;
 var playerAttack= 10;
 var playerMoney = 10;
 
-console.log(playerName, playerName, playerHealth);
+// console.log(playerName, playerName, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+// console.log(enemyNames);
+// console.log(enemyNames [0]);
+// console.log(enemyNames [1]);
+// console.log(enemyNames [2]);
+// console.log(enemyNames.length);
+
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
+
+var fight = function(enemyName) {
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     
@@ -18,8 +30,8 @@ var fight = function() {
         enemyHealth = enemyHealth - playerAttack;
         
         console.log(
-            playerName + " attacked" + " enemyName. " + "enemyName" + " now has " + enemyHealth + " health remaining."
-    );
+            playerName + " attacked" + " enemyName. "  +  enemyName + " now has " + enemyHealth + " health remaining.");
+;
 
     if (enemyHealth <= 0) {
         window.alert(enemyName + " has died!");
@@ -37,7 +49,7 @@ var fight = function() {
         window.alert(playerName + " has died!");
     }
     else { 
-        window.alert(playerName + " still has " + playerHealth + "health left.") 
+        window.alert(playerName + " still has " + playerHealth + " health left.") 
     }
 
 }   else if (promptFight === "skip" || promptFight === "SKIP") {
@@ -58,5 +70,6 @@ var fight = function() {
     }
 };
 
-fight();
-
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
